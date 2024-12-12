517597bee1f8bca4a1c84c01da9b10ca1ed11c69
Kostya Prodan <debaun4ry@gmail.com>
2024-12-12 17:55:42 +0200

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -1,98 +1,138 @@
-const carousel = document.querySelector('.posts__list');
-const leftButton = document.querySelector('.carousel__button--left');
-const rightButton = document.querySelector('.carousel__button--right');
-const items = document.querySelectorAll('.posts_list__item');
-let itemWidth = 0;
-let visibleItems = 0;
-const totalItems = items.length;
-let offset = 0;
+class Carousel {
+	constructor(containerSelector) {
+		this.carousel = document.querySelector(`${containerSelector} .posts__list`);
+		this.leftButton = document.querySelector(
+			`${containerSelector} .carousel__button--left`
+		);
+		this.rightButton = document.querySelector(
+			`${containerSelector} .carousel__button--right`
+		);
+		this.postsTitle = document.querySelector(
+			`${containerSelector} .posts__title`
+		);
+		this.postImage = document.querySelector(
+			`${containerSelector} .posts_list__item img`
+		);
+		this.items = document.querySelectorAll(
+			`${containerSelector} .posts_list__item`
+		);
 
-let startX = 0; // Начальная точка касания
-let currentX = 0; // Текущая точка касания
-let isDragging = false; // Флаг, активен ли свайп
+		this.itemWidth = 0;
+		this.visibleItems = 0;
+		this.totalItems = this.items.length;
+		this.offset = 0;
 
-function calculateVisibleItems() {
-	const containerWidth = carousel.parentElement.offsetWidth;
-	if (containerWidth <= 375) {
-		visibleItems = 1;
-	} else if (containerWidth <= 580) {
-		visibleItems = 2;
-	} else if (containerWidth <= 845) {
-		visibleItems = 3;
-	} else {
-		visibleItems = 4;
-	}
-	itemWidth = containerWidth / visibleItems;
-	return visibleItems;
-}
-function updateStyles() {
-	items.forEach(item => {
-		item.style.flex = `0 0 ${itemWidth - 18}px`;
-		item.style.maxWidth = `${itemWidth - 18}px`;
-	});
-	updateButtons();
-}
-function updateButtons() {
-	const maxOffset = -(itemWidth * (totalItems - visibleItems));
-	leftButton.disabled = offset === 0;
-	rightButton.disabled = offset <= maxOffset;
-}
-function moveCarousel(direction) {
-	if (direction === 'left') {
-		offset += itemWidth;
-		if (offset > 0) offset = 0;
-	} else if (direction === 'right') {
-		const maxOffset = -(itemWidth * (totalItems - visibleItems));
-		offset -= itemWidth;
-		if (offset < maxOffset) offset = maxOffset;
-	}
-	carousel.style.transform = `translateX(${offset}px)`;
-	updateButtons();
-}
-function handleResize() {
-	visibleItems = calculateVisibleItems();
-	offset = 0;
-	updateStyles();
-	carousel.style.transform = 'translateX(0px)';
-}
-leftButton.addEventListener('click', () => moveCarousel('left'));
-rightButton.addEventListener('click', () => moveCarousel('right'));
+		this.startX = 0;
+		this.currentX = 0;
+		this.isDragging = false;
 
-// Начало касания
-carousel.addEventListener('touchstart', e => {
-	startX = e.touches[0].clientX;
-	isDragging = true;
-	carousel.style.transition = 'none'; // Отключаем анимацию
-});
-
-// Движение пальца
-carousel.addEventListener('touchmove', e => {
-	if (!isDragging) return;
-
-	currentX = e.touches[0].clientX;
-	const deltaX = currentX - startX; // Разница между начальной и текущей позицией
-	carousel.style.transform = `translateX(${offset + deltaX}px)`; // Перемещаем контент
-});
-
-// Завершение свайпа
-carousel.addEventListener('touchend', () => {
-	if (!isDragging) return;
-
-	isDragging = false;
-	const deltaX = currentX - startX;
-
-	if (deltaX > 50) {
-		moveCarousel('left'); // Свайп вправо
-	} else if (deltaX < -50) {
-		moveCarousel('right'); // Свайп влево
-	} else {
-		// Если свайп незначительный, возвращаем карусель в исходное положение
-		carousel.style.transform = `translateX(${offset}px)`;
+		this.init();
 	}
 
-	carousel.style.transition = 'transform 0.5s ease-in-out'; // Включаем анимацию
-});
+	init() {
+		this.calculateVisibleItems();
+		this.updateStyles();
+		this.updateButtonPosition();
+		this.addEventListeners();
+	}
 
-window.addEventListener('resize', handleResize);
-handleResize();
-updateButtons();
+	calculateVisibleItems() {
+		const containerWidth = this.carousel.parentElement.offsetWidth;
+		if (containerWidth <= 375) {
+			this.visibleItems = 1;
+		} else if (containerWidth <= 580) {
+			this.visibleItems = 2;
+		} else if (containerWidth <= 845) {
+			this.visibleItems = 3;
+		} else {
+			this.visibleItems = 4;
+		}
+		this.itemWidth = containerWidth / this.visibleItems;
+	}
+
+	updateStyles() {
+		this.items.forEach(item => {
+			item.style.flex = `0 0 ${this.itemWidth - 18}px`;
+			item.style.maxWidth = `${this.itemWidth - 18}px`;
+		});
+		this.updateButtons();
+	}
+
+	updateButtons() {
+		const maxOffset = -(this.itemWidth * (this.totalItems - this.visibleItems));
+		this.leftButton.disabled = this.offset === 0;
+		this.rightButton.disabled = this.offset <= maxOffset;
+	}
+
+	moveCarousel(direction) {
+		const maxOffset = -(this.itemWidth * (this.totalItems - this.visibleItems));
+		if (direction === 'left') {
+			this.offset = Math.min(this.offset + this.itemWidth, 0);
+		} else if (direction === 'right') {
+			this.offset = Math.max(this.offset - this.itemWidth, maxOffset);
+		}
+		this.carousel.style.transform = `translateX(${this.offset}px)`;
+		this.updateButtons();
+	}
+
+	handleResize() {
+		this.calculateVisibleItems();
+		this.offset = 0;
+		this.updateStyles();
+		this.carousel.style.transform = 'translateX(0px)';
+		this.updateButtonPosition();
+	}
+
+	updateButtonPosition() {
+		const titleHeight = this.postsTitle.offsetHeight;
+		const imageHeight = this.postImage.offsetHeight;
+		const buttonTop = titleHeight + imageHeight / 2;
+		this.leftButton.style.top = `${buttonTop}px`;
+		this.rightButton.style.top = `${buttonTop}px`;
+	}
+
+	addTouchEvents() {
+		this.carousel.addEventListener('touchstart', e => {
+			this.startX = e.touches[0].clientX;
+			this.isDragging = true;
+			this.carousel.style.transition = 'none';
+		});
+
+		this.carousel.addEventListener('touchmove', e => {
+			if (!this.isDragging) return;
+
+			this.currentX = e.touches[0].clientX;
+			const deltaX = this.currentX - this.startX;
+			this.carousel.style.transform = `translateX(${this.offset + deltaX}px)`;
+		});
+
+		this.carousel.addEventListener('touchend', () => {
+			if (!this.isDragging) return;
+
+			this.isDragging = false;
+			const deltaX = this.currentX - this.startX;
+
+			if (deltaX > 50) {
+				this.moveCarousel('left');
+			} else if (deltaX < -50) {
+				this.moveCarousel('right');
+			} else {
+				this.carousel.style.transform = `translateX(${this.offset}px)`;
+			}
+
+			this.carousel.style.transition = 'transform 0.5s ease-in-out';
+		});
+	}
+
+	addEventListeners() {
+		this.leftButton.addEventListener('click', () => this.moveCarousel('left'));
+		this.rightButton.addEventListener('click', () =>
+			this.moveCarousel('right')
+		);
+		window.addEventListener('resize', () => this.handleResize());
+		this.addTouchEvents();
+	}
+}
+
+// Initialize the carousel
+new Carousel('.posts__block');
